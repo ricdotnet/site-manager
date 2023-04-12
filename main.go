962f0796434c9bab73ef00dad71894f131656975
Ricardo Rocha <me@rrocha.uk>
2023-04-12 23:01:04 +0100

    12/04/2023
    Added a script to read and map domain -> .conf files insite sites-available

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"github.com/op/go-logging"
 	"github.com/ricdotnet/goenvironmental"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 	router "ricr.dev/site-manager/api/v1"
 	"ricr.dev/site-manager/api/v1/sites"
 	"ricr.dev/site-manager/api/v1/user"
 	"ricr.dev/site-manager/config"
+	"ricr.dev/site-manager/scripts"
 )
 
 func main() {
 	goenvironmental.ParseEnv()
 	cfg := config.NewConfig()
+
+	command := os.Args[1]
+	if command == "sa" {
+		sitesAvailable(cfg.Logger)
+		return
+	}
 
 	dbString, _ := goenvironmental.Get("DB_STRING")
 	db, err := gorm.Open(mysql.Open(dbString), &gorm.Config{})
@@ -26,4 +36,11 @@ func main() {
 
 	v1 := router.New(cfg, db)
 	v1.Logger.Fatal(v1.Start(":4000"))
+}
+
+func sitesAvailable(l *logging.Logger) {
+	sitesMap := scripts.Init(l).MapSites()
+	for k, v := range sitesMap {
+		fmt.Printf("[key]: %s - [value]: %s\n", k, v)
+	}
 }
---
+++ scripts/sites_available.go
@@ -0,0 +1,64 @@
+package scripts
+
+import (
+	"bufio"
+	"github.com/op/go-logging"
+	"github.com/ricdotnet/goenvironmental"
+	"os"
+	"strings"
+)
+
+type sitesA map[string]string
+
+type SitesAvailable struct {
+	logger *logging.Logger
+}
+
+func Init(logger *logging.Logger) *SitesAvailable {
+	return &SitesAvailable{
+		logger: logger,
+	}
+}
+
+func (sa *SitesAvailable) MapSites() sitesA {
+	sa.logger.Info("Entering mapSites")
+
+	apacheDir, err := goenvironmental.Get("APACHE_DIR")
+	if err != nil {
+		sa.logger.Fatal("Could not read the APACHE_DIR env variable")
+		return nil
+	}
+
+	sitesAvailable := make(sitesA)
+
+	files, _ := os.ReadDir(apacheDir + "/sites-available")
+	for _, file := range files {
+		if file.Name() != ".DS_Store" {
+			sa.parseFile(apacheDir+"/sites-available/", file.Name(), sitesAvailable)
+		}
+	}
+
+	sa.logger.Infof("Finished parsing and mapping a total of %d files", len(files))
+	return sitesAvailable
+}
+
+func (sa *SitesAvailable) parseFile(path string, fileName string, sitesA sitesA) {
+	sa.logger.Infof("Started parsing %s", fileName)
+
+	lines, _ := os.Open(path + fileName)
+
+	reader := bufio.NewScanner(lines)
+	reader.Split(bufio.ScanLines)
+	for reader.Scan() {
+		pair := strings.Split(strings.TrimSpace(reader.Text()), " ")
+		if pair[0] == "ServerName" {
+			sa.logger.Infof("Mapping %s to %s\n", fileName, pair[1])
+			sitesA[fileName] = pair[1]
+			break
+		}
+	}
+
+	lines.Close()
+
+	sa.logger.Infof("Finished parsing %s", fileName)
+}
