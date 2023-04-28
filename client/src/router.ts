b14bb92e665cc495e7c5b3d548b7f7faf47476c7
Ricardo Rocha <me@rrocha.uk>
2023-04-28 20:26:08 +0100

    Update router.ts
    Spacing...

--- client/src/router.ts
+++ client/src/router.ts
@@ -4,7 +4,7 @@ import Login from "./pages/Login.vue";
 import Register from "./pages/Register.vue";
 
 const routes = [
-  {path: '/', component: Home},
+  { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/register', component: Register }
 ]
