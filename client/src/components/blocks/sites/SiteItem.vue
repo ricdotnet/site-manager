04c1d9df198efffc9bf01c830ab5a9154508df12
Ricardo Rocha <me@rrocha.uk>
2023-07-06 21:52:28 +0100

    SM-e9b3e
    Added monaco-editor and a vite plugin.

--- client/src/components/blocks/sites/SiteItem.vue
+++ client/src/components/blocks/sites/SiteItem.vue
@@ -4,13 +4,35 @@
     <div>Here you can edit your vhosts config file linked to this domain.
       When saving your site will be updated with the new configs.</div>
 
-<!--    {{ getSite() }}-->
+    <div ref="monacoRef" class="h-[400px]"></div>
+    {{ getSite() }}
+
+    <Button type="button" name="save" color="primary" text="Save" @click="onClick"/>
   </div>
 </template>
 
 <script setup lang="ts">
   import { useSitesStore } from "@stores";
+  import * as monaco from 'monaco-editor';
+  import { onMounted, ref } from "vue";
+  import Button from "../../UI/Button.vue";
 
   const sitesStore = useSitesStore();
   const { getSite } = sitesStore;
+
+  const monacoRef = ref<HTMLElement>();
+
+  onMounted(() => {
+    if (monacoRef.value) {
+      monaco.editor.create(monacoRef.value, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+        language: 'text',
+        theme: 'vs-dark',
+      });
+    }
+  });
+
+  const onClick = () => {
+    console.log(monaco.editor.getEditors()[0].getValue().split('\n'));
+  }
 </script>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from "path";
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: './dist'
   },
-  plugins: [vue()],
+  plugins: [vue(), monacoEditorPlugin.default({
+    languageWorkers: ['json', 'editorWorkerService'],
+  })],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
