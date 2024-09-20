5e5ce63bcd3d326f7ea4300a14069adaf115fad3
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-09-20 13:36:09 +0100

    add a simple useEvents composable

--- client/src/composables/index.ts
+++ client/src/composables/index.ts
@@ -2,4 +2,5 @@ export { useTheme } from './useTheme.ts';
 export { useAuth } from './useAuth.ts';
 export { useRequest } from './useRequest.ts';
 export { useDebounce } from './useDebounce.ts';
+export { useEvents } from './useEvents.ts';
 export { useEditor } from './useEditor.ts';
