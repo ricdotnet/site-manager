5c468030f825e05f003ee616c6c59d763d5d78d0
Ricardo Rocha <me@rrocha.uk>
2024-10-23 15:03:13 +0100

    add eslint and update github actions to lint instead of biome

---
+++ client/eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strict,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  { ignores: ['node_modules/', 'dist/'] },
+];
