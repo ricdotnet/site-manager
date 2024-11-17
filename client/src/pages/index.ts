32d19181ffb944926c6843f9b5bf6cda59468f18
Ricardo Rocha <me@rrocha.uk>
2024-11-17 17:51:07 +0000

    remove register routes and pages

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,7 +1,6 @@
 export { default as NotFound } from './NotFound.vue';
 export { default as Home } from './Home.vue';
 export { default as Login } from './Login.vue';
-export { default as Register } from './Register.vue';
 export { default as Dashboard } from './Dashboard.vue';
 export { default as Overview } from './Overview.vue';
 export { default as Sites } from './Sites.vue';
--- server/api/v1/user/routes.go
+++ server/api/v1/user/routes.go
@@ -11,8 +11,6 @@ func Routes(v1 *echo.Group, db *gorm.DB) {
 
 	user := v1.Group("/user")
 
-	//user.POST("/login", userApi.loginUser)
-	//user.POST("/register", userApi.registerUser)
 	user.PATCH("/update", userApi.updateUser, middlewares.CookieMiddleware(db))
 	user.GET("/auth", userApi.authUser, middlewares.CookieMiddleware(db))
 	user.GET("/sessions", userApi.getSessions, middlewares.CookieMiddleware(db))
